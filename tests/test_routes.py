a318d1d5035fc9de0f155fcf1263d826b903a64b
Vince Rezula <vrezula@austin.rr.com>
2026-07-01 16:50:36 -0400

    96 percent and lint at 10.0 . 2 working tests.

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -171,10 +171,10 @@ class TestProductRoutes(TestCase):
         response = self.client.get(f"{BASE_URL}/{test_product.id}")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         data = response.get_json()
-        # self.assertEqual(data["name"], test_product.name)
+        self.assertEqual(data["name"], test_product.name)
 
     def test_get_product_not_found(self):
-        """It should retrieve a product"""
+        """It should not find an id=0 product"""
         response = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
